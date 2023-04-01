b76e10457b5752667dc6802160efb6870a267343
최서윤 <ruirun@choeseoyun-ui-MacBookAir.local>
2023-04-01 13:37:02 +0900

    [Feat] Presenter 구현

--- src/Page/Todo/TodoPresenter.js
+++ src/Page/Todo/TodoPresenter.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function TodoPresenter() {
   return (
-    <div>TodoPresenter</div>
+    <div className='todoList'>
+        <div className='header'>To Do List</div>
+        <div className='date'></div>
+        <div className='list'></div>
+        <form className='addList' action="/add" method="POST">
+            <input type="text" className='addListContents' name="todo" placeholder="오늘 할 일" />
+            <button type="submit" className='addListBtn'>추가</button>
+        </form>
+    </div> 
   )
 }
 
